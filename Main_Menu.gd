9ee293d370a50536333d96b316100e87e20c1a3c
AWildman128 <alexanderwildman@letu.edu>
2022-10-13 18:15:11 -0500

    Added basic layout of lobby list and lobby

---
+++ Main_Menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Singleplayer_pressed():
+	pass # Replace with function body.
+
+#Goes to lobby list
+func _on_Multiplayer_pressed():
+	get_tree().change_scene("res://Lobby_List.tscn")
+
+
+func _on_Tutorial_pressed():
+	pass # Replace with function body.
+
+
+func _on_Credits_pressed():
+	pass # Replace with function body.
