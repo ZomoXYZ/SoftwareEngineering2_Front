23333bc0ebf32adeb9b2f16b5270b7154b68cbae
Jake <jakerosch@letu.edu>
2022-10-28 10:12:23 -0500

    updated HTTPAuth again again

--- HTTPAuth.gd
+++ HTTPAuth.gd
@@ -12,94 +12,28 @@ var PlayerNameAdjective = -1
 var PlayerNameNoun = -1
 var PlayerPicture = -1
 
-func createClient():
-	var client = HTTPClient.new()
-	var err = client.connect_to_host(Host, Port)
-	if err != OK:
-		print("Offline")
+func requestNoToken(endpoint, method = HTTPClient.METHOD_GET, body = null, headers = []):
+	headers.append("UUID: %s" % OS.get_unique_id())
+	var request = HTTPAuthClass.request(endpoint, method, body, headers, 1)
+	if request == HTTPAuthClass.Status.Offline:
+		print("OFFLINE")
 		return null
+	return request
 
-	# Wait until resolved and connected.
-	while client.get_status() == HTTPClient.STATUS_CONNECTING || client.get_status() == HTTPClient.STATUS_RESOLVING:
-		client.poll()
-		print("Connecting...")
-		if not OS.has_feature("web"):
-			OS.delay_msec(500)
-		else:
-			yield(Engine.get_main_loop(), "idle_frame")
-
-	if client.get_status() == HTTPClient.STATUS_CANT_CONNECT || client.get_status() == HTTPClient.STATUS_CANT_RESOLVE || client.get_status() == HTTPClient.STATUS_CONNECTION_ERROR || client.get_status() == HTTPClient.STATUS_SSL_HANDSHAKE_ERROR:
-		print("Offline")
-		return null
-	
-	assert(client.get_status() == HTTPClient.STATUS_CONNECTED)
-	return client
-
-func request(path, method = HTTPClient.METHOD_GET, body = null, noToken = false):
-	assert(noToken || token != "")
-	var client = createClient()
-	if client == null:
-		return null
-	# Some headers
-	var headers = [ "UUID: %s" % OS.get_unique_id() ]
-	if !noToken:
-		headers.append("Authorization: %s" % token)
-		
-	var bodyStr = ""
-	if body != null:
-		bodyStr = JSON.print(body)
-
-	var err = client.request(method, path, headers, bodyStr)
-	assert(err == OK)
-
-	# wait for request to complete
-	while client.get_status() == HTTPClient.STATUS_REQUESTING:
-		client.poll()
-		print("Requesting...")
-		if OS.has_feature("web"):
-			yield(Engine.get_main_loop(), "idle_frame")
-		else:
-			OS.delay_msec(500)
-
-	assert(client.get_status() == HTTPClient.STATUS_BODY or client.get_status() == HTTPClient.STATUS_CONNECTED)
-
-	if client.has_response():
-		headers = client.get_response_headers_as_dictionary() 
-		print("code: ", client.get_response_code())
-		print("**headers:\\n", headers)
-
-		# buffer
-		var rb = PoolByteArray()
-
-		# fill buffer with chunks
-		while client.get_status() == HTTPClient.STATUS_BODY:
-			client.poll()
-			var chunk = client.read_response_body_chunk()
-			if chunk.size() == 0:
-				if not OS.has_feature("web"):
-					OS.delay_usec(1000)
-				else:
-					yield(Engine.get_main_loop(), "idle_frame")
-			else:
-				rb = rb + chunk
-
-		print("bytes got: ", rb.size())
-		var text = rb.get_string_from_ascii()
-		print("Text: ", text)
-		return JSON.parse(text)
-		
-	return null
+func request(endpoint, method = HTTPClient.METHOD_GET, body = null, headers = []):
+	assert(token != "")
+	headers.append("Authorization: %s" % token)
+	return requestNoToken(endpoint, method, body, headers)
 
 func getUserData():
 	var file = File.new()
-	file.open("user://save_game.dat", File.READ)
+	file.open("user://userdata.dat", File.READ)
 	var content = file.get_as_text()
 	file.close()
 	var jsonResult = JSON.parse(content)
 	if jsonResult.error != OK || !jsonResult.result.has('name') || !jsonResult.result.name.has('adjective') || !jsonResult.result.name.has('noun') || !jsonResult.result.has('picture'):
 		return null
 	return jsonResult.result
-	
 
 func setUserData(data):
 	assert(data.has('name') && data.name.has('adjective') && data.name.has('noun') && data.has('picture'))
@@ -110,12 +44,11 @@ func setUserData(data):
 		"picture": picture
 	})
 	var file = File.new()
-	file.open("user://save_game.dat", File.WRITE)
+	file.open("user://userdata.dat", File.WRITE)
 	file.store_string(content)
 	file.close()
 	
 func authorizeSession(force = false):
-	
 	# only run every 15 seconds at most
 	if force || (lastChecked != -1 && lastChecked > Time.get_ticks_msec() - 15 * 1000):
 		print("Checking too quick")
@@ -127,9 +60,10 @@ func authorizeSession(force = false):
 		print("TODO confirm token")
 		return
 	
-	var jsonResult = request("/authorization", HTTPClient.METHOD_GET, null, true)
+	var jsonResult = requestNoToken("/authorization")
 	if jsonResult == null:
 		return
+	
 	assert(jsonResult.error == OK && !jsonResult.result.has("error"))
 	token = jsonResult.result.token
 	
---
+++ HTTPAuthClass.gd
@@ -0,0 +1,80 @@
+extends Node
+
+const Schema = "http"
+const Host = "localhost"
+const Port = 8080
+const Path = ""
+const BASE_URL = "%s://%s:%s%s" % [Schema, Host, Port, Path]
+
+enum Status {Online, Offline}
+
+# timeout is in seconds
+func request(endpoint, method = HTTPClient.METHOD_GET, body = null, headers = null, timeout = 1):
+	var client = HTTPClient.new()
+	var err = client.connect_to_host(Host, Port)
+	if err != OK:
+		print("Offline")
+		return Status.Offline
+
+	var timer = 0
+	while (client.get_status() == HTTPClient.STATUS_CONNECTING || client.get_status() == HTTPClient.STATUS_RESOLVING) && timer < timeout * 10:
+		client.poll()
+		print("Connecting...")
+		timer+=1
+		if not OS.has_feature("web"):
+			OS.delay_msec(100)
+		else:
+			yield(Engine.get_main_loop(), "idle_frame")
+
+	if timer >= timeout * 10:
+		return Status.Offline
+	
+	var bodyStr = ""
+	if body != null:
+		bodyStr = JSON.print(body)
+
+	err = client.request(method, BASE_URL + endpoint, headers, bodyStr)
+	if err != OK:
+		return Status.Offline
+
+	while client.get_status() == HTTPClient.STATUS_REQUESTING && timer < timeout * 10:
+		client.poll()
+		print("Requesting...")
+		timer+=1
+		if OS.has_feature("web"):
+			yield(Engine.get_main_loop(), "idle_frame")
+		else:
+			OS.delay_msec(500)
+
+	if timer >= timeout * 10:
+		return Status.Offline
+
+	if client.get_status() != HTTPClient.STATUS_BODY && client.get_status() != HTTPClient.STATUS_CONNECTED:
+		return Status.Offline
+
+	if client.has_response():
+		headers = client.get_response_headers_as_dictionary() 
+		print("code: ", client.get_response_code())
+		print("**headers:\\n", headers)
+
+		# buffer
+		var rb = PoolByteArray()
+
+		# fill buffer with chunks
+		while client.get_status() == HTTPClient.STATUS_BODY:
+			client.poll()
+			var chunk = client.read_response_body_chunk()
+			if chunk.size() == 0:
+				if not OS.has_feature("web"):
+					OS.delay_usec(100)
+				else:
+					yield(Engine.get_main_loop(), "idle_frame")
+			else:
+				rb = rb + chunk
+
+		print("bytes got: ", rb.size())
+		var text = rb.get_string_from_ascii()
+		print("Text: ", text)
+		return JSON.parse(text)
+		
+	return null
