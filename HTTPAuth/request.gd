caf70ba83f1a5e12f930e04ff7f5bc3853c4a0be
Jake <jakerosch@letu.edu>
2022-11-10 17:46:11 -0600

    accidentally changed remote data

--- HTTPAuth/request.gd
+++ HTTPAuth/request.gd
@@ -1,10 +1,10 @@
 extends Node
 
-const Schema_http = "http"
-const Schema_ws = "ws"
-const Host = "localhost"
-const Port = 8080
-const Path = ""
+const Schema_http = "https"
+const Schema_ws = "wss"
+const Host = "wan.zomo.dev"
+const Port = 80
+const Path = "/api/v1"
 const BASE_URL = "%s://%s:%s%s" % [Schema_http, Host, Port, Path]
 const WS_URL = "%s://%s:%s%s/ws" % [Schema_ws, Host, Port, Path]
 
