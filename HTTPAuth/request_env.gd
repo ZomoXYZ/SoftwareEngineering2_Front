7aa0fabe750f5ccc28e783b7b97236a7546e0f6f
Jake <jakerosch@letu.edu>
2022-11-11 10:20:57 -0600

    copilot led me astray :crying_emoji:

--- HTTPAuth/request_env.gd
+++ HTTPAuth/request_env.gd
@@ -1,16 +1,16 @@
 extends Node
 
-const Schema_http = "https"
-const Schema_ws = "wss"
-const Host = "wan.zomo.dev"
-const Port = 443
-const Path = "/api/v1"
+# const Schema_http = "https"
+# const Schema_ws = "wss"
+# const Host = "wan.zomo.dev"
+# const Port = 443
+# const Path = "/api/v1"
 
-# const Schema_http = "http"
-# const Schema_ws = "ws"
-# const Host = "localhost"
-# const Port = 8080
-# const Path = ""
+const Schema_http = "http"
+const Schema_ws = "ws"
+const Host = "localhost"
+const Port = 8080
+const Path = ""
 
 const BASE_URL = "%s://%s:%s%s" % [Schema_http, Host, Port, Path]
 const WS_URL = "%s://%s:%s%s/ws" % [Schema_ws, Host, Port, Path]
