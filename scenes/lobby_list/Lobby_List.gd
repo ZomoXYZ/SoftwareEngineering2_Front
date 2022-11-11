991117b7d8aaf5d73e40d737419c1c0babbd1a71
AWildman128 <alexanderwildman@letu.edu>
2022-11-10 18:01:52 -0600

    made lobby scene switch on join

--- scenes/lobby_list/Lobby_List.gd
+++ scenes/lobby_list/Lobby_List.gd
@@ -13,7 +13,8 @@ func _ready():
 	# my vote is to not allow this page to be acceessed at all
 	if Request.token != "":
 		Request.createRequest(self, "_on_get_lobbylyst", "/lobbylist")
-		
+	
+	LobbyConn.connect("joined_lobby", self, "_lobby_joined")
 	#Standard animation procedure
 	$IntroPanel.show()
 	$AnimationPlayer.play("Intro_Transition")
